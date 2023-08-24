24f027a90a547f774f9022a9382c0a5aa5d29132
CoralieMa <coralie.martinot@gmail.com>
2023-08-24 10:51:11 +0200

    ex02 done

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,11 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cmartino <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: cmartino <cmartino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 10:07:18 by cmartino          #+#    #+#             */
-/*   Updated: 2023/08/23 10:07:19 by cmartino         ###   ########.fr       */
+/*   Updated: 2023/08/24 10:45:28 by cmartino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
+#include <iostream>
 
+int	main(void){
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << "Memory address of str: " << &str << std::endl;
+	std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
+	std::cout << "Value of str: " << str << std::endl;
+	std::cout << "Value pointed by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value pointed by stringREF: " << stringREF << std::endl;
+}
