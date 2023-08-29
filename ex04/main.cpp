c3dadd68fcfddaeb6e9de3b9220962c22170dbd3
CoralieMa <coralie.martinot@gmail.com>
2023-08-29 16:19:37 +0200

    begin ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -3,11 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cmartino <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: cmartino <cmartino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 10:09:04 by cmartino          #+#    #+#             */
-/*   Updated: 2023/08/23 10:09:06 by cmartino         ###   ########.fr       */
+/*   Updated: 2023/08/24 13:50:27 by cmartino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <fstream>
+#include <iostream>
 
+int	main(int argc, char **argv){
+	std::string	fileName;
+	std::string	fileCopy;
+	std::string	s1;
+	std::string	s2;
+	
+	if (argc != 4)
+	{
+		std::cout << "Wrong input" << std::endl;
+		return (0);
+	}
+	fileName = argv[1];
+	fileCopy = fileName + ".replace";
+	s1 = argv[2];
+	s2 = argv[3];
+
+	std::ifstream	ifs(fileName);
+	if (ifs == 0)
+	{
+		std::cout << "File: " << fileName << " does not exist" << std::endl;
+		exit (0);
+	}
+	std::ofstream	ofs(fileCopy);
+
+
+
+	
+	ofs << "test" << std::endl;
+	return (0);
+}
