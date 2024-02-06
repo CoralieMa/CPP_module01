c036c8ec3ecf32f6aa9b3ce4fa7a179cecc9d87d
CoralieMa <coralie.martinot@gmail.com>
2024-02-06 14:40:44 +0100

    ex04 first try

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,43 +6,62 @@
 /*   By: cmartino <cmartino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 10:09:04 by cmartino          #+#    #+#             */
-/*   Updated: 2024/02/06 11:23:32 by cmartino         ###   ########.fr       */
+/*   Updated: 2024/02/06 14:38:47 by cmartino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>  
 #include <fstream>
 #include <iostream>
 
 
-int replace(std::string fileName, std::string s1, std::string s2)
+
+int ft(std::string fileName, std::string s1, std::string s2)
 {
 	std::string 	buffer;
 	size_t			pos;
+	size_t			i;
 	std::ifstream	ifs(fileName);
 
 	if (ifs)
 	{
 		std::ofstream	ofs(fileName + ".replace");
-		// while (getline(ifs, buffer))
-		// {
-		// 	find()		
-		// 	if ()
-		// }
+		while (getline(ifs, buffer))
+		{
+			pos = buffer.find(s1);
+			i = 0;
+			if (pos == -1)
+				ofs << buffer << std::endl;
+			else
+			{
+				while (pos != -1)
+				{
+					while (i < pos)
+					{
+						ofs << buffer[i];
+						++i;
+					}
+					ofs << s2;
+					i += s2.length() + 1;
+					pos = buffer.find(s1, i);
+				}
+				ofs << &buffer[i] << std::endl;
+			}
+		}
 	}
 	else
 	{
 		std::cout << "File: " << fileName << " does not exist" << std::endl;
 		return (1);
 	}
-	
-	// ofs << "test" << std::endl;
 	return (0);
 }
 
 int	main(int argc, char **argv){
+	std::cout << argv[2];
 	if (argc != 4)
 		std::cout << "Wrong input" << std::endl;
 	else
-		return (replace(argv[1], argv[2], argv[3]));
+		return (ft(argv[1], argv[2], argv[3]));
 	return (1);
 }
